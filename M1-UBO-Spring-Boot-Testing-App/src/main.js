85d34b414ffc17eb1d0c700a4dd40d1aec7c094e
Steven <116876919+StevenTld@users.noreply.github.com>
2025-03-05 17:56:25 +0100

    Reset

--- M1-UBO-Spring-Boot-Testing-App/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/styles.css'
-
-// Create and mount the Vue application
-createApp(App)
-  .use(router)
-  .mount('#app')
